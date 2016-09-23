54aca1471aa54a469bc4eb61de38f9c6c953ee22
Fixxy <csszone@gmail.com>
2016-09-23 17:15:51 +0300

    added initial INSERT query

--- imdb_parser/MySQL.cs
+++ imdb_parser/MySQL.cs
@@ -20,8 +20,6 @@ namespace imdb_parser
                                   ";database="  + db +
                                   ";userid="    + username +
                                   ";password="  + password;
-
-            
             dbcon = new MySqlConnection(connectMysql);
             dbcon.Open();
         }
@@ -29,28 +27,51 @@ namespace imdb_parser
         public void upload(string movieID, string title, string year, List<string> genresList, string description, Dictionary<string, string> starsList, Dictionary<string, string> directorsList, List<string> stillsList)
         {
             IDbCommand dbcmd = dbcon.CreateCommand();
-            string mysqlQuery = "SELECT id, gid, title, directors, actors, year, description FROM imdb_test.films WHERE id > 0";
+            string genresComma = "";
+            string actorsComma = "";
+            string directorsComma = "";
+
+            //combine all genres in one string
+            for (int i = 0; i < genresList.Count; i++) { genresComma += genresList.ElementAt(i) + ",";  }
+            //combine all actors in one string
+            for (int i = 0; i < starsList.Count; i++) { actorsComma += starsList.Values.ElementAt(i) + ","; }
+            //combine all directors in one string
+            for (int i = 0; i < directorsList.Count; i++) { directorsComma += directorsList.Values.ElementAt(i) + ","; }
+
+            //add a new film to imdb_test.films
+            string mysqlQuery = "INSERT INTO imdb_test.films (gid, title, directors, actors, year, description, genres) VALUES ('"
+                + movieID + "', '"
+                + title + "', '"
+                + directorsComma.TrimEnd(',') + "', '"
+                + actorsComma.TrimEnd(',') + "', "
+                + year + ", '"
+                + description + "', '"
+                + genresComma.TrimEnd(',') + "');";
+
+            dbcmd.CommandText = mysqlQuery;
+            dbcmd.ExecuteNonQuery();
+
+/*            string mysqlQuery = "SELECT id, gid, title, directors, actors, year, description FROM imdb_test.films WHERE id > 0";
             dbcmd.CommandText = mysqlQuery;
 
-
-/*            IDataReader reader = dbcmd.ExecuteReader();
+            IDataReader reader = dbcmd.ExecuteReader();
             while (reader.Read())
             {
                 Console.WriteLine("---------------------------------------------");
-                
+
                 string id = (string)reader["id"].ToString();
                 string gid = (string)reader["gid"].ToString();
-                string year = (string)reader["year"].ToString();
-                string title = (string)reader["title"].ToString();
+                string year1 = (string)reader["year"].ToString();
+                string title1 = (string)reader["title"].ToString();
                 string actors = (string)reader["actors"].ToString();
                 string directors = (string)reader["directors"].ToString();
-                string description = (string)reader["description"].ToString();
+                string description1 = (string)reader["description"].ToString();
 
-                Console.WriteLine("[debug] {0} {1} {2} {3} {4} {5} {6}", id, gid, year, title, actors, directors, description);
+                Console.WriteLine("[debug] {0} {1} {2} {3} {4} {5} {6}", id, gid, year1, title1, actors, directors, description1);
             }
             reader.Close();
-            reader = null;
-*/
+            reader = null; */
+
             // clean up the mysql connection
             dbcmd.Dispose();
             dbcmd = null;
